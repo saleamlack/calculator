c7405c6292f39e48129491c3b6c4bfb4d1045a94
Saleamlack <saleamlack7@gmail.com>
2023-03-05 15:51:30 +0300

    Create object for Mathematical operations

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ let calculatorInput = {
     secondOperand: null,
     currentOperand: null,
 }
+
+let calculatorOperations = {
+    "+": (firstOperand, secondOperand = 0) => {
+        return +firstOperand + +secondOperand;
+    },
+    "-": (firstOperand, secondOperand) => {
+        return firstOperand - secondOperand;
+    },
+    "*": (firstOperand, secondOperand) => {
+        return firstOperand * secondOperand;
+    },
+    "/": (firstOperand, secondOperand) => {
+        if (secondOperand != 0) {
+            return firstOperand / secondOperand;
+        }
+        alert("Error: can't divide to 0");
+        clear();
+    }
+}
